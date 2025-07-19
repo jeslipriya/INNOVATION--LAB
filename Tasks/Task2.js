6586b13ad5957ad7486e94e7cbfd1f493596df8b
Jesli <jeslipriya07@gmail.com>
2025-07-19 21:03:43 +0530

    added task 2

--- Tasks/Task2.js
+++ Tasks/Task2.js
@@ -13,6 +13,18 @@ while (num <=10) {
 let password;
 do {
     password = prompt("Enter the password: ");
-    
+
 } while (password !== "PassWord");
 console.log("Access Granted!");
+
+// Sum of first N natural numbers using for loop
+
+let N = prompt("Enter a number to find it's sum: ");
+let sum = 0;
+
+for(i=0; i<=N; i++){
+    sum += i;
+}
+
+console.log("Sum = " + sum);
+
