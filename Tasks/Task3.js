17490d1d1711305e4a05db801269d97421fa5833
Jesli <jeslipriya07@gmail.com>
2025-07-20 16:54:30 +0530

    added task 3

--- Tasks/Task3.js
+++ Tasks/Task3.js
@@ -2,3 +2,18 @@
 
 // Exiting the while loop using break on finding the first prime number
 
+let num = 10;
+while (true) {
+    let isPrime = true;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log("First prime number is:", num);
+        break;
+    }
+    num++;
+}
