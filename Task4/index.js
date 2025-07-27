ead779d8c8308800d9a3c1f1c7dab1faafe9a2f1
Jesli <jeslipriya07@gmail.com>
2025-07-27 16:34:58 +0530

    Added

--- Task4/index.js
+++ Task4/index.js
@@ -1,11 +1,18 @@
 //1) Reverse word in a sentence
 
-let word = "jesli is a IronMan";
-let reverse = "";
-for(i = word.length-1; i>=0; i--){
-    reverse = reverse + word[i];
-}
+// let word = "jesli is a IronMan";
+// let reverse = "";
+// for(i = word.length-1; i>=0; i--){
+//     reverse = reverse + word[i];
+// }
 
-console.log(reverse);
+// console.log(reverse);
 
 //2) Remove duplicate in a sentence
+
+let arr = [1,2,3,4,2,4,8];
+
+let s = new Set(arr);
+let noduplicate = [...s];
+
+console.log(noduplicate);
