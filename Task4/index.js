4ae0dac97d590a3eb95904fb5afc9f1cd13d733d
Jesli <jeslipriya07@gmail.com>
2025-07-27 16:43:39 +0530

    updated

--- Task4/index.js
+++ Task4/index.js
@@ -10,9 +10,30 @@
 
 //2) Remove duplicate in a sentence
 
-let arr = [1,2,3,4,2,4,8];
+// let arr = [1,2,3,4,2,4,8];
 
-let s = new Set(arr);
-let noduplicate = [...s];
+// let s = new Set(arr);
+// let noduplicate = [...s];
 
-console.log(noduplicate);
+// console.log(noduplicate);
+
+
+// 3) count vowels in a string
+
+let vowels = ["A","E","I","O","U","a","e","i","o","u"];
+
+let sentence = "Welcome to JavaScript Programming";
+
+let temp ="";
+for(i = sentence.length-1; i>=0; i--){
+    if(vowels.includes(sentence[i])){
+        continue;
+    }
+    
+    else{
+        temp += sentence[i];
+    }
+
+}
+
+console.log(temp);
---
+++ Task4/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,2,4,8];
+
+// let s = new Set(arr);
+// let noduplicate = [...s];
+
+// console.log(noduplicate);
