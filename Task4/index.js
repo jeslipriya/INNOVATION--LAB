6153f02afdf3072eb5e6eb3180abe488b503b260
Jesli <jeslipriya07@gmail.com>
2025-07-27 16:51:24 +0530

    updated

--- Task4/index.js
+++ Task4/index.js
@@ -1,6 +1,6 @@
 //1) Reverse word in a sentence
 
-let word = "jesli is a IronMan";
+let word = "Welcome to JavaScript Programming";
 let reverse = "";
 for(i = word.length-1; i>=0; i--){
     reverse = reverse + word[i];
