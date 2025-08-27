8466bcd34290b44bf96cb6b7888e102705abfb14
Jesli <jeslipriya07@gmail.com>
2025-08-27 21:11:26 +0530

    Added

--- Task6/index.js
+++ Task6/index.js
@@ -81,3 +81,20 @@ contacts = [...contacts, ...moreContacts1, ...moreContacts2];
 
 console.log("\nAll Contacts:");
 contacts.forEach(contact => contact.displayInfo());
+
+function showContactInfo(city, country) {
+    console.log(`Name: ${this.name}, Phone: ${this.phone}, Email: ${this.email}, Location: ${city}, ${country}`);
+}
+
+// call()
+console.log("\nUsing call():");
+showContactInfo.call(contacts[0], "New York", "USA");
+
+// apply()
+console.log("\nUsing apply():");
+showContactInfo.apply(contacts[1], ["London", "UK"]);
+
+// bind()
+console.log("\nUsing bind():");
+let boundShow = showContactInfo.bind(contacts[2], "Paris", "France");
+boundShow();
